2b19e64bbb6c29869fefef146251d60ed582c3b9
LuigisLuigi <74737916+MaxMaginnis@users.noreply.github.com>
2022-10-28 10:01:31 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
+window.addEventListener("load", (e) => {
+    for (let element in document.getElementsByClassName("secret")) {
+        element.onclick = (e) => {
+            if (e.button != 0)
+                return;
 
+            //TODO make popup window to enter password
+        }
+    }
+});
