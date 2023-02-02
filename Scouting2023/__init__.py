4d59f8588660dd1d97f8640a3071adcc97b60190
SZB3748 <cubepiggy@gmail.com>
2023-02-01 23:44:52 -0500

    Added route to get all assets that sw.js needs to cache
    Making a GET request to /assets with return a JSON array of all the urls that need to be cached, from templates, static, and otherwise.

--- Scouting2023/__init__.py
+++ Scouting2023/__init__.py
@@ -1,5 +1,6 @@
 from . import competition
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, url_for
+import json
 import os
 import traceback
 import waitress #production quality WSGI server to host the flask app with. more: https://docs.pylonsproject.org/projects/waitress/en/stable/index.html
@@ -11,6 +12,14 @@ app = Flask(__name__, static_folder=STATIC, template_folder=TEMPLATES)
 app.url_map.strict_slashes = False
 
 comps = []
+not_content = []
+
+def not_content_route(rule:str, onto=app, **options):
+    "Mark route as a not-content-returning route. Internally uses route decorator with the object passed to onto. Default is app."
+    def decor(f):
+        not_content.append(rule)
+        return onto.route(rule, **options)(f)
+    return decor
 
 #define routes
 #for info on decorators, see https://realpython.com/primer-on-python-decorators/, or look up "python decorators"
@@ -31,8 +40,35 @@ def help():
 def compselect():
     return render_template("compselect.html")
 
+#api routes
+#cite: https://stackoverflow.com/a/13318415
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+def _get_static_routes(dir=STATIC, name="static")->"list[str]":
+    rtv = []
+    for filename in os.listdir(dir):
+        path = os.path.join(dir, filename)
+        if os.path.isdir(path):
+            rtv.extend(_get_static_routes(path, name=name+"/"+filename))
+        elif os.path.isfile(path):
+            rtv.append(f"/{name}/{filename}")
+    return rtv
+
+@not_content_route("/assets")
+def assets():
+    routes = list({
+        url_for(rule.endpoint, **(rule.defaults or {}))
+        for rule in app.url_map.iter_rules()
+        if "GET" in rule.methods and has_no_empty_params(rule) and url_for(rule.endpoint, **(rule.defaults or {})) not in not_content
+    })
+    return json.dumps(routes+_get_static_routes())
+
 
 #functions
+
 def serve(host:str="localhost", port:int=8080):
     "Serve the webapp."
     print("Serving", host, f"on port {port}.")
