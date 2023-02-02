d845036fe1b65baf85d8718ebe06d3683de291bc
SZB3748 <cubepiggy@gmail.com>
2023-02-02 09:20:02 -0500

    Add upload demo code

--- comps/comps2023/data_manage.py
+++ comps/comps2023/data_manage.py
@@ -1,2 +1,10 @@
+import json
+from PIL import Image
+from pyzbar import pyzbar
+
 def parse_qr_code(fp):
-    ...
+    decoded:list = pyzbar.decode(Image.open(fp))
+    return json.loads(decoded[0].data.decode("ascii"))
+
+if __name__ == "__main__":
+    print(parse_qr_code(r"C:\Users\tickl\Downloads\download.png"))
